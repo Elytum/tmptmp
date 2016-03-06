d12902207d351a84f86f800a0e1c6a319c252576
U-GOP-LINK\a.chazal <a.chazal@LGOP267.gop.link>
2016-03-06 03:17:37 +0100

    autocommit

--- app/assets/javascripts/views/order-view.js
+++ app/assets/javascripts/views/order-view.js
@@ -6,7 +6,7 @@ var OrderView = Marionette.ItemView.extend({
 		//el: '#tweet-container',
 		template: require('../../templates/order.hbs'),
 
-		events: function(){
+		events: {
 			'click #start-place' : 'showMap'
 		},
 
