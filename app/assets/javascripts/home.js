888a72a41309aec62caa8afab3ff17a85e5a078e
U-GOP-LINK\a.chazal <a.chazal@LGOP267.gop.link>
2016-03-06 02:29:50 +0100

    autocommit

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,51 +3,7 @@
 
 //Data
 //Angular App Module and Controller
-         origin = '41 rue Jussieu';
-              arrcities = [];
-              n = 0;
-              var map;
-              var createMarker = function (info){
 
-                  var marker = new google.maps.Marker({
-                      map: map,
-                      position: new google.maps.LatLng(info.lat, info.long),
-                      title: info.city
-                  });
-                  marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-
-                  google.maps.event.addListener(marker, 'click', function(){
-                      infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-                      infoWindow.open($scope.map[n], marker);
-                  });
-
-              }
-              function initMap() {
-                var mapDiv = document.getElementById('map');
-                map = new google.maps.Map(mapDiv, {
-                  center: {lat: 44.540, lng: -78.546},
-                  zoom: 8
-                });
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", 'http://api-adresse.data.gouv.fr/search/?q='+origin, false);
-                xhr.send();
-                jsonified = JSON.parse(xhr.response);
-                longitude = jsonified.features[0].geometry.coordinates[0];
-                latitude = jsonified.features[0].geometry.coordinates[1];
-                xhr.open("GET", 'https://datanova.laposte.fr/api/records/1.0/search/?dataset=laposte_poincont2&geofilter.distance=' + latitude + ',' + longitude + ',5000', false);
-                xhr.send();
-                res = JSON.parse(xhr.response).records;
-                for (var ids in res) {
-                    tmp1 = res[ids].fields.latitude - latitude;
-                    tmp2 = res[ids].fields.longitude - longitude;
-                    dist = Math.sqrt(tmp1 * tmp1 + tmp2 * tmp2);
-                    tmp = {id: res[ids].fields.libelle_du_site.identifiant_a, city: res[ids].fields.libelle_du_site, desc: res[ids].fields.adresse + " "  + res[ids].fields.code_postal + ", "  + res[ids].fields.localite, lat: res[ids].fields.latitude, long: res[ids].fields.longitude, dist: dist};
-                    arrcities.push(tmp);
-                    createMarker(tmp);
-                }
-                console.log(arrcities);
-              }
-              initMap();
 
 
 arrcities = [[],[]];
